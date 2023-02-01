3a4e493ff270dbfeab081998ef40fc26fdef48e2
Akshay <Akshaysanap2123@gmail.com>
2023-02-01 20:03:46 +0530

    value change in sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum
     public static void main(String[] args)
     {
         int a = 5;
-        int b = 8;
+        int b = 15;
         int c = a + b;
 
         System.out.println("Sum = a + b : " +c);
