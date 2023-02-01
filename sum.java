6415459fcad105e60fe757b94169bf7dbd68d517
Akshay <Akshaysanap2123@gmail.com>
2023-02-01 19:59:57 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 8;
+        int c = a + b;
+
+        System.out.println("Sum = a + b : " +c);
+    }
+}
